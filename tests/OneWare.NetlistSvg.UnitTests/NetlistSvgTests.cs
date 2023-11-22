6c851d7b2a82e77fba60b73105ba94f18865cbc1
Hendrik Mennen <hmennen@protop-solutions.com>
2023-11-22 18:13:48 +0100

    case

---
+++ tests/OneWare.NetlistSvg.UnitTests/NetlistSvgTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using OneWare.Shared.Services;
+using Prism.Ioc;
+using Xunit;
+
+namespace OneWare.NetlistSvg.UnitTests;
+
+public class NetlistSvgTests
+{
+    [Fact]
+    public void LoadLibrary()
+    {
+        //TODO
+    }
+}
